e8e5bb3ae7b2071be94c0a7139a859e81844b0cd
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2026-02-01 06:29:59 +0000

    ek bar sab app dekh lijiye ki sab kuchh achhi tarah se kam kar raha hai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/logo.svg" />
         <meta name="theme-color" content="#000000" />
       </head>
       <body>
